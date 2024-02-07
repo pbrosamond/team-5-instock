405bb0ab6a36f76635f4d12c44bdbb1d1876f980
pbrosamond <pbrosamond@gmail.com>
2024-02-06 22:28:52 -0800

    added a search mixin

--- src/components/WarehouseList/WarehouseList.js
+++ src/components/WarehouseList/WarehouseList.js
@@ -1,34 +1,38 @@
 import "./WarehouseList.scss";
-import 
+import sort from "../../assets/icons/sort-default-24px.svg";
 
 import { Link } from "react-router-dom";
 
 function WarehouseList() {
   return (
-    <section>
-      <h1>Warehouses</h1>
-      <search></search>
-      <button></button>
+    <section className="warehouse">
+      <div className="warehouse__header">
+        <h1 className="warehouse__title">Warehouses</h1>
+        <input className="warehouse__search" placeholder="Search"></input>
+        <button className="warehouse__button">+ Add New Warehouse</button>
+      </div>
 
-      <div>
-        <h4>WAREHOUSE</h4>
-        <img></img>
-      </div>
-      <div>
-        <h4>ADDRESS</h4>
-        <img></img>
-      </div>
-      <div>
-        <h4>CONTACT NAME</h4>
-        <img></img>
-      </div>
-      <div>
-        <h4>CONTACT INFORMATION</h4>
-        <img></img>
-      </div>
-      <div>
-        <h4>ACTIONS</h4>
-      </div>
+      <section>
+        <div>
+          <h4>WAREHOUSE</h4>
+          <img src={sort}></img>
+        </div>
+        <div>
+          <h4>ADDRESS</h4>
+          <img src={sort}></img>
+        </div>
+        <div>
+          <h4>CONTACT NAME</h4>
+          <img src={sort}></img>
+        </div>
+        <div>
+          <h4>CONTACT INFORMATION</h4>
+          <img src={sort}></img>
+        </div>
+        <div>
+          <h4>ACTIONS</h4>
+        </div>
+      </section>
     </section>
   );
 }
