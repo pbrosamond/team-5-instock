eda955df15d023ab11a565f9e6d9679f0963d11f
pbrosamond <pbrosamond@gmail.com>
2024-02-06 22:05:04 -0800

    Working on Warehouse list and item html

--- src/components/Warehouse Item/WarehouseItem.js
+++ src/components/Warehouse Item/WarehouseItem.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 function WarehouseItem() {
   return (
     <div>
+      
     </div>
   );
 }
--- src/components/WarehouseList/WarehouseList.js
+++ src/components/WarehouseList/WarehouseList.js
@@ -1,11 +1,35 @@
 import "./WarehouseList.scss";
+import 
 
 import { Link } from "react-router-dom";
 
 function WarehouseList() {
   return (
-    <div>
-    </div>
+    <section>
+      <h1>Warehouses</h1>
+      <search></search>
+      <button></button>
+
+      <div>
+        <h4>WAREHOUSE</h4>
+        <img></img>
+      </div>
+      <div>
+        <h4>ADDRESS</h4>
+        <img></img>
+      </div>
+      <div>
+        <h4>CONTACT NAME</h4>
+        <img></img>
+      </div>
+      <div>
+        <h4>CONTACT INFORMATION</h4>
+        <img></img>
+      </div>
+      <div>
+        <h4>ACTIONS</h4>
+      </div>
+    </section>
   );
 }
 
