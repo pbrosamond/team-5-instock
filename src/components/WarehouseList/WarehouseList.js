32868ef327a7a2f8ce014dd43a090f209b6b6ea1
pbrosamond <pbrosamond@gmail.com>
2024-02-06 22:48:35 -0800

    fixed background color of footer to light-grey

--- src/components/WarehouseList/WarehouseList.js
+++ src/components/WarehouseList/WarehouseList.js
@@ -8,29 +8,29 @@ function WarehouseList() {
     <section className="warehouse">
       <div className="warehouse__header">
         <h1 className="warehouse__title">Warehouses</h1>
-        <input className="warehouse__search" placeholder="Search"></input>
+        <input className="warehouse__search" placeholder="Search..."></input>
         <button className="warehouse__button">+ Add New Warehouse</button>
       </div>
 
-      <section>
-        <div>
-          <h4>WAREHOUSE</h4>
+      <section className="warehouse__filter">
+        <div className="warehouse__sorts">
+          <h4 className="warehouse__subtitle">WAREHOUSE</h4>
           <img src={sort}></img>
         </div>
-        <div>
-          <h4>ADDRESS</h4>
+        <div className="warehouse__sorts">
+          <h4 className="warehouse__subtitle">ADDRESS</h4>
           <img src={sort}></img>
         </div>
-        <div>
-          <h4>CONTACT NAME</h4>
+        <div className="warehouse__sorts">
+          <h4 className="warehouse__subtitle">CONTACT NAME</h4>
           <img src={sort}></img>
         </div>
-        <div>
-          <h4>CONTACT INFORMATION</h4>
+        <div className="warehouse__sorts">
+          <h4 className="warehouse__subtitle">CONTACT INFORMATION</h4>
           <img src={sort}></img>
         </div>
-        <div>
-          <h4>ACTIONS</h4>
+        <div className="warehouse__sorts">
+          <h4 className="warehouse__subtitle">ACTIONS</h4>
         </div>
       </section>
     </section>
