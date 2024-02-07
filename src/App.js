004978f2e4b7ecc99e498eb6701212fda86eb90f
鄧莉瀕 <k18382003@gmail.com>
2024-02-06 23:19:35 -0800

    PO23-22 Front-End: Delete Warehouse Modal/Component (Responsive)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import "./App.scss";
+import './App.scss';
 
-import Header from "./components/Header/Header";
-import WarehousePage from "./pages/WarehousePage/WarehousePage";
-import InventoryPage from "./pages/InventoryPage/InventoryPage";
-import Footer from "./components/Footer/Footer";
+import Header from './components/Header/Header';
+import WarehousePage from './pages/WarehousePage/WarehousePage';
+import InventoryPage from './pages/InventoryPage/InventoryPage';
+import Footer from './components/Footer/Footer';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
--- src/components/WarehouseDelete/WarehouseDelete.js
+++ src/components/WarehouseDelete/WarehouseDelete.js
@@ -1,11 +1,29 @@
-import "./WarehouseDelete.scss";
+import './WarehouseDelete.scss';
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function WarehouseDelete() {
   return (
-    <div>
-    </div>
+    <>
+      <div className="overlay"></div>
+      <div className="delete">
+        <div className="delete__message-container">
+          <h1 className="delete__title">Delete Washington warehouse?</h1>
+          <p className="delete__message">
+            Please confirm that you’d like to delete the Washington from the
+            list of warehouses. You won’t be able to undo this action.
+          </p>
+        </div>
+        <div className="delete__button-container">
+          <button className="delete__button delete__button-cancel">
+            Cancel
+          </button>
+          <button className="delete__button delete__button-delete">
+            Delete
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
