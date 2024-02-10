bc3729d419f66a0bba618ee7cb4e68a9523d807d
sbodnarek-neff <153647204+sbodnarek-neff@users.noreply.github.com>
2024-02-09 18:58:08 -0800

    Revert "background color + route add"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.scss';
 
-import WarehouseAdd from './components/WarehouseAdd/WarehouseAdd';
 import Header from "./components/Header/Header";
 import WarehousePage from "./pages/WarehousePage/WarehousePage";
 import WarehouseEditPage from "./pages/WarehouseEditPage/WarehouseEditPage";
