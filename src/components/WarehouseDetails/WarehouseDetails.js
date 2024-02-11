4b70ce6abea9cdfc0161cc853ffa08d18a65a985
pbrosamond <pbrosamond@gmail.com>
2024-02-11 10:12:45 -0800

    updated WarehouseDetails Page

--- src/components/WarehouseDetails/WarehouseDetails.js
+++ src/components/WarehouseDetails/WarehouseDetails.js
@@ -17,12 +17,12 @@ function WarehouseDetails({ id, warehouse, inventoryList }) {
       <main className="item__card">
         <div className="item__header">
           <div className="item__header-container">
-            <Link to={`/warehouses`}><button className="item__back-button">
+            <Link className="item__back-button--link" to={`/warehouses`}><button className="item__back-button">
               <img src={backArrow} />
             </button></Link>
             <h1 className="item__name">{warehouse.warehouse_name}</h1>
           </div>
-          <Link to={`/warehouses/${id}`}><button className="item__edit-button">
+          <Link className="item__edit-button--link" to={`/warehouses/${id}`}><button className="item__edit-button">
             <img src={editButton} />
             <p className="item__edit-button-word">Edit</p>
           </button></Link>
@@ -31,7 +31,7 @@ function WarehouseDetails({ id, warehouse, inventoryList }) {
         <div className="item__container">
           <section className="item__wrapper item__left-container">
             <div className="item__block">
-              <h4 className="item__subtitle">WAREHOUSE ADDRESS:</h4>
+              <h4 className="item__subtitle">WAREHOUSE ADDRESS</h4>
               <p>{warehouse.address}</p>
               <p>
                 {warehouse.city}, {warehouse.country}
@@ -41,12 +41,12 @@ function WarehouseDetails({ id, warehouse, inventoryList }) {
           <section className="item__wrapper item__right-container">
             <div className="item__wrap">
               <div className="item__block">
-                <h4 className="item__subtitle">CONTACT NAME:</h4>
+                <h4 className="item__subtitle">CONTACT NAME</h4>
                 <p>{warehouse.contact_name}</p>
                 <p>{warehouse.contact_position}</p>
               </div>
               <div className="item__block">
-                <h4 className="item__subtitle">CONTACT INFORMATION:</h4>
+                <h4 className="item__subtitle">CONTACT INFORMATION</h4>
                 <p>{warehouse.contact_phone}</p>
                 <p>{warehouse.contact_email}</p>
               </div>
@@ -68,7 +68,7 @@ function WarehouseDetails({ id, warehouse, inventoryList }) {
             <img src={sort}></img>
           </div>
           <div className="warehouse__sorts">
-            <h4 className="warehouse__subtitle">QUANTITY</h4>
+            <h4 className="warehouse__subtitle">QTY</h4>
             <img src={sort}></img>
           </div>
           <div className="warehouse__sorts actions">
